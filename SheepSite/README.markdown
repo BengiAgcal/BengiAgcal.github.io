d3d248d4c2b00965fabf27e11c9bf467d4866925
BengiAgcal <66956789+BengiAgcal@users.noreply.github.com>
2021-11-04 17:10:57 +0800

    Add files via upload
    initial commit

---
+++ SheepSite/README.markdown
@@ -0,0 +1,5 @@
+# Three.js - Background Cubemap
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/bagcal/pen/wvqoybd](https://codepen.io/bagcal/pen/wvqoybd).
+
+from: https://threejsfundamentals.org/threejs/threejs-background-cubemap.html
---
+++ SheepSite/dist/script.js
@@ -0,0 +1,129 @@
+// Three.js - Background Cubemap
+// from https://threejsfundamentals.org/threejs/threejs-background-cubemap.html
+
+
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/build/three.module.js';
+import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r132/examples/jsm/controls/OrbitControls.js';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+import Stats from 'three/examples/jsm/libs/stats.module'
+
+function main() {
+  const canvas = document.querySelector('#c');
+  const renderer = new THREE.WebGLRenderer({canvas});
+
+  const fov = 75;
+  const aspect = 2;  // the canvas default
+  const near = 0.1;
+  const far = 100;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 3;
+
+  const controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 0, 0);
+  controls.update();
+
+  const scene = new THREE.Scene();
+
+  {
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
+
+  const fbxLoader = new FBXLoader()
+  fbxLoader.load(
+    'https://res.cloudinary.com/bagcal/raw/upload/v1634912776/shep_dzbs1u.fbx',
+    (object) => {
+        // object.traverse(function (child) {
+        //     if ((child as THREE.Mesh).isMesh) {
+        //         // (child as THREE.Mesh).material = material
+        //         if ((child as THREE.Mesh).material) {
+        //             ((child as THREE.Mesh).material as THREE.MeshBasicMaterial).transparent = false
+        //         }
+        //     }
+        // })
+        // object.scale.set(.01, .01, .01)
+        scene.add(object)
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
+
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+  function makeInstance(geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({color});
+
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    cube.position.x = x;
+
+    return cube;
+  }
+
+  const cubes = [
+    makeInstance(geometry, 0x44aa88,  0),
+    makeInstance(geometry, 0x8844aa, -2),
+    makeInstance(geometry, 0xaa8844,  2),
+  ];
+
+  {
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.load([
+      'https://res.cloudinary.com/bagcal/image/upload/v1634893552/snowalps_lf_e9ycdy.png',
+      'https://res.cloudinary.com/bagcal/image/upload/v1634893552/snowalps_rt_vnatkm.png',
+      'https://res.cloudinary.com/bagcal/image/upload/v1634895401/snowalps_up_ki0tgr.png',
+      'https://res.cloudinary.com/bagcal/image/upload/a_270/v1634893552/snowalps_dn_utdez6.png',
+      'https://res.cloudinary.com/bagcal/image/upload/v1634893552/snowalps_ft_xpldil.png',
+      'https://res.cloudinary.com/bagcal/image/upload/v1634893552/snowalps_bk_zqn7ry.png',
+    ]);
+    scene.background = texture;
+
+  }
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  function render(time) {
+    time *= 0.001;
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * .1;
+      const rot = time * speed;
+      cube.rotation.x = rot;
+      cube.rotation.y = rot;
+    });
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+}
+
+main();
